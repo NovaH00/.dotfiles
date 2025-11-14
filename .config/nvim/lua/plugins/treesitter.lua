124469aa8bc44c80e21dccfa4d699fa03eca84c5
NovaH00 <trantay2006super@gmail.com>
2025-11-14 18:20:59 +0700

    disable treesitter indent

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
             require("nvim-treesitter.configs").setup({
                 ensure_installed = { "lua","c","python","cpp","rust","javascript","typescript" },
                 highlight = { enable = true, additional_vim_regex_highlighting = false },
-                indent = { enable = true },
+                indent = { enable = false },
             })
         end,
     },
