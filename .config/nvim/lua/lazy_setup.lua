acd0df63c4e5d357c0f33d4c4fdcc72a76988706
NovaH00 <trantay2006super@gmail.com>
2025-11-15 00:27:54 +0700

    added tab switch for tmux and minor fix for nvim

--- .config/nvim/lua/lazy_setup.lua
+++ .config/nvim/lua/lazy_setup.lua
@@ -8,4 +8,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {})
+require("lazy").setup("plugins", {
+  rocks = {
+    hererocks = true,  -- enable automatic Hererocks installation
+    enabled = true,    -- enable LuaRocks support
+  }
+})
